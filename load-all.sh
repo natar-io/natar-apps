53e56caf00d3e77a1d832a709ef0fcf9b362872e
Jeremy Laviole <poqudrof@gmail.com>
2018-07-04 14:01:55 +0200

    Load inverted cam-proj extrinsics.

--- load-all.sh
+++ load-all.sh
@@ -5,5 +5,5 @@ prog=target/nectar-configuration-loader-0.1-SNAPSHOT-jar-with-dependencies.jar
 java -jar $prog -f data/calibration-AstraS-depth.yaml -o "calibration:camera:astraS-depth" -pd
 java -jar $prog -f data/calibration-AstraS-rgb.yaml -pd -o camera0:calibration
 java -jar $prog -f data/projector.yaml -pd -pr -o projector0:calibration
-java -jar $prog -f data/camProjExtrinsics.xml -m -o projector0:extrinsics
+java -jar $prog -f data/camProjExtrinsics.xml -m -i -o projector0:extrinsics
 java -jar $prog -f data/tablePosition.xml -m -o scene:tablePosition
