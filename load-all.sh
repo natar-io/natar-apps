76b57a9f08219b9db5719685cbbbd0d9edb07712
Nicolas PALARD <palardnicolas@gmail.com>
2018-07-06 14:03:26 +0200

    :zap: Updated log script

--- load-all.sh
+++ load-all.sh
@@ -2,8 +2,16 @@
 
 prog=target/nectar-configuration-loader-0.1-SNAPSHOT-jar-with-dependencies.jar
 
-java -jar $prog -f data/calibration-AstraS-depth.yaml -o "calibration:camera:astraS-depth" -pd
+#RGB Camera
 java -jar $prog -f data/calibration-AstraS-rgb.yaml -pd -o camera0:calibration
-java -jar $prog -f data/projector.yaml -pd -pr -o projector0:calibration
-java -jar $prog -f data/camProjExtrinsics.xml -m -i -o projector0:extrinsics
-java -jar $prog -f data/tablePosition.xml -m -o scene:tablePosition
+
+#Depth Camera
+java -jar $prog -f data/calibration-AstraS-depth.yaml -o camera0:calibration:depth -pd
+java -jar $prog -f data/calibration-AstraS-stereo.xml -o camera0:extrinsics:depth -m
+
+#Projector
+java -jar $prog -f data/projector.yaml -o projector0:calibration -pd -pr
+java -jar $prog -f data/camProjExtrinsics.xml -o projector0:extrinsics -m -i
+
+#Table
+java -jar $prog -f data/tablePosition.xml -m -o camera0:table:position
